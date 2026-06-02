aeae2d1b9baf4c813ff60be13a097dd9c675db38
JOAO VITOR HELIO COSTA MAIA <joao.vhcmaia@senacsp.edu.br>
2026-06-02 12:01:37 -0300

    Fazendo correções gerais de desempenho e semantica

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,15 @@
 
 const heroVideo = document.getElementsByTagName("video")[0];
 const titulo = document.querySelector('#titulo');
-const tituloNav = document.querySelector('#titulo-nav');
-const ancoraComoFunciona = document.querySelector('#ancora-como-funciona');
 const botaoRolagem = document.querySelector('#botao-rolar');
 heroVideo.playbackRate = 0.75;
-console.log(botaoRolagem);
-const footer = document.querySelector('.pronto-para-transformar');
 
 function esmaecerElemento(elementoObservado, elementoAlterado){
     const elementoResponsivo = new IntersectionObserver (([entry]) => {
-        if (entry.isIntersecting){
-            elementoAlterado.classList.add('visivel');
-        } else  elementoAlterado.classList.remove('visivel');
-        {
-            threshold: 0;
-        }
-});
-elementoResponsivo.observe(elementoObservado)
+        elementoAlterado.classList.toggle('visivel', entry.isIntersecting);
+    },{threshold: 0 });
+    elementoResponsivo.observe(elementoObservado)
 }
 
-// esmaecerElemento(titulo, tituloNav)
 
 esmaecerElemento(titulo, botaoRolagem)
